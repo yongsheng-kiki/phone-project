a82715a68ccfe05841ca55614729d38381c8c5c4
yongsheng-kiki <lys_yongsheng@126.com>
2016-06-17 13:57:13 +0800

    移动端

---
+++ js/index.js
@@ -0,0 +1,65 @@
+//->动态计算REM的换算比例
+~function (desW) {
+    var winW = document.documentElement.clientWidth;
+    document.documentElement.style.fontSize = winW / desW * 100 + "px";
+}(640);
+
+//->给滑屏区域进行初始化设置
+~function () {
+    var step = 0, divList = null;
+    var swp = new Swiper(".swiper-container", {
+        loop: true,
+        direction: 'vertical',
+        effect : 'flip',
+        onSlidePrevEnd: function () {
+            step--;
+            change();
+            if (step === 0) {
+                step = 4;
+            }
+        },
+        onSlideNextEnd: function () {
+            step++;
+            change();
+            if (step === 5) {
+                step = 1;
+            }
+        }
+    });
+
+    function change() {
+        divList = document.querySelectorAll(".swiper-slide");
+        [].forEach.call(divList, function (curDiv, index) {
+            curDiv.id = index === step ? curDiv.getAttribute("trueId") : null;
+        });
+    }
+
+}();
+
+
+(function(){
+    var audioBox = document.querySelector(".audio"),
+        myAudio=audioBox.getElementsByTagName("audio")[0];
+    window.setTimeout(function(){
+        myAudio.play();
+        myAudio.addEventListener("canplay",function(){
+            audioBox.style.display="block";
+            audioBox.className+= " audioMove";
+        },false);
+    },1000);
+
+    audioBox.addEventListener("click",function(){
+        if(myAudio.paused){
+            myAudio.play();
+            audioBox.className+= "audio audioMove";
+            return;
+        }
+        myAudio.pause();
+        audioBox.className+= "audio";
+    },false);
+})();
+
+
+
+
+
